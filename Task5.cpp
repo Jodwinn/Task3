429ed8a5a75c3495ab13674c3979e5c7a7f97d3e
Jodwinn <pavelkharkovski@gmail.com>
2024-09-21 14:52:24 +0300

    Create Task5.cpp

---
+++ Task5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main() {
+	int a = 3;
+	int b = a * 3;
+	int c = a + b;
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+}
